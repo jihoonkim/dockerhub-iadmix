57ac33f25f6516ea1243a0a6ffb7d51e131f0881
Jihoon Kim <j5kim@ucsd.edu>
2016-06-07 12:20:02 -0700

    fix the file checking error

--- workflow.sh
+++ workflow.sh
@@ -13,17 +13,16 @@ export INPUT_PREFIX=$1
 export iADMIX_DIR=/opt/iADMIX-v0.2
 
 
-### If both PLINK format files, .ped and .map, exist, 
-###  run vctools and iadmix sequentially.
-if [ -f ${INPUT_PREFIX}.ped ] && [ -f ${INPUT_PREFIX}.map ]; then
+### If either of PLINK format files, .ped and .map, does NOT exist, 
+###  run vctools to create conversion files from .vcf
+if [ ! -f ${INPUT_PREFIX}.ped ] && [ ! -f ${INPUT_PREFIX}.map ]; then
    ### convert vcf to PLINK format (.ped and .map)
    vcftools --vcf ${INPUT_PREFIX}.vcf --plink --out ${INPUT_PREFIX}
-
-   ### compute population allele frequencies 
-   python ${iADMIX_DIR}/runancestry.py               \
-     --freq  ${iADMIX_DIR}/hapmap3.8populations.hg19 \
-     --path=${iADMIX_DIR}                            \
-     --plink ${INPUT_PREFIX}                         \
-     --out   ${INPUT_PREFIX}    
 fi
 
+### compute population allele frequencies 
+python ${iADMIX_DIR}/runancestry.py               \
+  --freq  ${iADMIX_DIR}/hapmap3.8populations.hg19 \
+  --path=${iADMIX_DIR}                            \
+  --plink ${INPUT_PREFIX}                         \
+  --out   ${INPUT_PREFIX}   
