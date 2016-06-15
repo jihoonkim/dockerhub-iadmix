1cc9826a7c0f7a389e197fac6db5a8dbb4de8729
Jihoon Kim <j5kim@ucsd.edu>
2016-06-15 16:27:40 -0700

    revise workflow.sh to load run.cfg

--- runexample.sh
+++ runexample.sh
@@ -1,12 +1,12 @@
 #-----------------------------------------------------------------------------
 #  File name : runexample.sh
 #  Author    : Jihoon Kim
-#  Last update : 06/07/2016
+#  Last update : 06/15/2016
 #  Description : Runs a workflow script for iAdmix docker
 #                        (https://hub.docker.com/r/j5kim/iadmix)
 #                to calculate population allele frequency for an input .vcf.
 #                Downloads a real, example .vcf data from NCBI and calls a
-#                workflow script, already preloaded in the idadmix docker 
+#                workflow script, already preloaded in the iadmix docker 
 #                above.
 # 
 #  Note:  FOR YOUR OWN ANALYSIS, REPLACE THE EXAMPLE INPUT WITH YOUR OWN 
@@ -18,16 +18,19 @@
 #              run as
 #              $ docker run -t -i -v /Users/JohnDoe/MyDockers/iAdmix:/mydata \
 #                  j5kim/iadmix:latest /bin/bash /opt/workflow.sh  \
-#                  /mydata/NA12878_GIAB
+#                  /mydata/runinput/run.cfg 
 #-----------------------------------------------------------------------------
-# make a /mydata directory if not exists.change a directory 
+# make a /mydata directory if not exists.
 mkdir -p /mydata
 
-# change a current directory to /mydata
-cd /mydata
+# make two subdirectories, runinput and runoutput, under /mydata
+mkdir -p /mydata/runinput
+mkdir -p /mydata/runoutput
 
-# download input data for a test run
-if [ !  -f /mydata/NA12878_GIAB.vcf ]; then
+
+# download input data, if not exist, for a test run
+cd /mydata/runinput
+if [ !  -f /mydata/runinput/NA12878_GIAB.vcf ]; then
    # download an example input file (.vcf) from NCBI
    wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/latest/supplementaryfiles/NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid_ALLCHROM_v3.2_all.vcf.gz
 
@@ -38,6 +41,12 @@ if [ !  -f /mydata/NA12878_GIAB.vcf ]; then
    mv NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid_ALLCHROM_v3.2_all.vcf NA12878_GIAB.vcf
 fi
 
+# create a configuration file for a run
+echo -e "export RUN_INPUT_DIR=/mydata/runinput"   >  /mydata/runinput/run.cfg
+echo -e "export RUN_OUTPUT_DIR=/mydata/runoutput" >> /mydata/runinput/run.cfg
+
+
 # run the workflow with the preloaded workflow.sh script
-# input is the prefix of the .vcf file without file extension
-bash /opt/workflow.sh /mydata/NA12878_GIAB
+# input is the configuration file for a run
+bash /opt/workflow.sh /mydata/runinput/run.cfg
+
--- workflow.sh
+++ workflow.sh
@@ -1,28 +1,41 @@
 #-----------------------------------------------------------------------------
 #  File name : runexample.sh
 #  Author    : Jihoon Kim
-#  Last update : 06/07/2016
+#  Last update : 06/15/2016
 #  Description : A workflow script for iAdmix docker 
 #                    (https://hub.docker.com/r/j5kim/iadmix)
 #                to calculate population allele frequency on input .vcf file.
 #                /opt     --> a directory for workflow.sh
-#                /mydata  --> a directory for user data   
+#                /mydata  --> a directory for a configuration file for a run,
+#                             which sets directories for input and output
 #-----------------------------------------------------------------------------
-### create an environment variable for sample .vcf file to shorten the input file
-export INPUT_PREFIX=$1
+### take the run-time configuration file, run.cfg
+source $1
+shift
+
+
 export iADMIX_DIR=/opt/iADMIX-v0.2
 
+### for each input .vcf file
+for INPUT_VCF in ${RUN_INPUT_DIR}/*.vcf 
+do
+    # remove both path and file extension, '.vcf'
+	INPUT_PREFIX=${INPUT_VCF##*/}
+	INPUT_PREFIX=${INPUT_PREFIX%.*}
+	OUTPUT_PREFIX=${RUN_OUTPUT_DIR}/${INPUT_PREFIX}
 
-### If either of PLINK format files, .ped and .map, does NOT exist, 
-###  run vctools to create conversion files from .vcf
-if [ ! -f ${INPUT_PREFIX}.ped ] && [ ! -f ${INPUT_PREFIX}.map ]; then
-   ### convert vcf to PLINK format (.ped and .map)
-   vcftools --vcf ${INPUT_PREFIX}.vcf --plink --out ${INPUT_PREFIX}
-fi
+	### If either of PLINK format files, .ped and .map, does NOT exist, 
+	###  run vctools to create conversion files from .vcf
+	if [ ! -f ${OUTPUT_PREFIX}.ped ] && [ ! -f ${OUTPUT_PREFIX}.map ]; then
+	   ### convert vcf to PLINK format (.ped and .map)
+	   vcftools --vcf ${INPUT_VCF} --plink  --out ${OUTPUT_PREFIX}
+	fi
 
-### compute population allele frequencies 
-python ${iADMIX_DIR}/runancestry.py               \
-  --freq  ${iADMIX_DIR}/hapmap3.8populations.hg19 \
-  --path=${iADMIX_DIR}                            \
-  --plink ${INPUT_PREFIX}                         \
-  --out   ${INPUT_PREFIX}   
+	### compute population allele frequencies 
+	python ${iADMIX_DIR}/runancestry.py               \
+	  --freq  ${iADMIX_DIR}/hapmap3.8populations.hg19 \
+	  --path=${iADMIX_DIR}                            \
+	  --plink ${OUTPUT_PREFIX}       \
+	  --out   ${OUTPUT_PREFIX}   
+
+done
