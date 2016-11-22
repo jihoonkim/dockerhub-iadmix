3169f2c75d664322b3dca9f15f167de605c0c871
jihoonkim <j5kim@ucsd.edu>
2016-11-22 15:06:43 -0800

    simplified the test run script

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 #-----------------------------------------------------------------------------
 #  File name : provision.sh
 #  Author    : Jihoon Kim
-#  Last update : 06/07/2016
+#  Last update : 11/22/2016
 #  Description : Proviosions iAdmix docker
 #                        (https://hub.docker.com/r/j5kim/iadmix)
 #                to calculate population allele frequency for an input .vcf.
@@ -14,8 +14,8 @@ apt-get update -y
 
 ### install dependent packages
 apt-get install -y  autoconf build-essential git g++ libncurses5-dev \
- libssl-dev make pkg-config software-properties-common python wget zip zlibc \
- zlib1g zlib1g-dev 
+ libssl-dev make pkg-config software-properties-common python vim \
+ wget zip zlibc zlib1g zlib1g-dev 
 
 ### install vcftools to convert .vcf to PLINK format file
 cd /opt
@@ -29,18 +29,38 @@ make install
 
 ### install iADMIX to compute population allele frequencies
 cd /opt 
-wget https://sites.google.com/site/vibansal/software/iAdmix/iADMIX-v0.2.zip 
-unzip iADMIX-v0.2.zip 
-cd /opt/iADMIX-v0.2
+#wget https://sites.google.com/site/vibansal/software/iAdmix/iADMIX-v0.2.zip 
+#unzip iADMIX-v0.2.zip 
+git clone https://github.com/vibansal/ancestry.git
+cd ancestry
+make all
+
+
+### download resource data, the population allele frequencies for common SNPs
+###  from the International HapMap project (ftp://ftp.ncbi.nlm.nih.gov/hapmap)
+cd /opt/ancestry
 wget https://sites.google.com/site/vibansal/software/iAdmix/hapmap3.8populations.hg19.zip 
 unzip hapmap3.8populations.hg19.zip
 rm hapmap3.8populations.hg19.zip
 
 
-### make a directory for user data
-mkdir -p /mydata 
+### make a directory for workflow and download a workflow script
+mkdir -p /workflow 
+wget https://raw.githubusercontent.com/jihoonkim/dockerhub-iadmix/master/workflow.sh
 
-### CLEAN-UP
-mv /opt/dockerhub-iadmix/runexample.sh /opt
-mv /opt/dockerhub-iadmix/workflow.sh /opt
-rm /opt/dockerhub-iadmix/Dockerfile
+
+### make a directory for a test run
+mkdir -p /testrun 
+cd /testrun
+# download a subset, chr22 only, of a public whole genome sequencing file
+# How the subset file was created using vcftools (https://vcftools.github.io)  
+# wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/NISTv3.3.1/GRCh37/HG001_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-X_v.3.3.1_highconf_phased.vcf.gz
+# gunzip HG001_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-X_v.3.3.1_highconf_phased.vcf.gz
+# mv HG001_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-X_v.3.3.1_highconf_phased.vcf HG001.vcf
+# vcftools --vcf HG001.vcf --chr 22 --recode --out HG001_chr22.vcf
+wget https://raw.githubusercontent.com/jihoonkim/dockerhub-iadmix/master/HG001_chr22.vcf 
+wget https://raw.githubusercontent.com/jihoonkim/dockerhub-iadmix/master/testrun.sh
+
+
+### make a directory for a user run
+mkdir -p /mydata 
--- workflow.sh
+++ workflow.sh
@@ -1,41 +1,22 @@
+#!/bin/bash 
+
 #-----------------------------------------------------------------------------
-#  File name : runexample.sh
+#  File name : workflow.sh
 #  Author    : Jihoon Kim
-#  Last update : 06/15/2016
+#  Last update : 11/22/2016
 #  Description : A workflow script for iAdmix docker 
 #                    (https://hub.docker.com/r/j5kim/iadmix)
 #                to calculate population allele frequency on input .vcf file.
-#                /opt     --> a directory for workflow.sh
-#                /mydata  --> a directory for a configuration file for a run,
-#                             which sets directories for input and output
 #-----------------------------------------------------------------------------
-### take the run-time configuration file, run.cfg
-source $1
-shift
+export INPUT_VCF=$1
+export RESOURCE_DATA=$2
+export iADMIX_DIR=$3
 
+export OUTPUT_PREFIX=${INPUT_VCF}
 
-export iADMIX_DIR=/opt/iADMIX-v0.2
+### convert .vcf to PLINK format (.ped and .map)
+vcftools --vcf ${INPUT_VCF} --plink  --out ${OUTPUT_PREFIX}
 
-### for each input .vcf file
-for INPUT_VCF in ${RUN_INPUT_DIR}/*.vcf 
-do
-    # remove both path and file extension, '.vcf'
-	INPUT_PREFIX=${INPUT_VCF##*/}
-	INPUT_PREFIX=${INPUT_PREFIX%.*}
-	OUTPUT_PREFIX=${RUN_OUTPUT_DIR}/${INPUT_PREFIX}
-
-	### If either of PLINK format files, .ped and .map, does NOT exist, 
-	###  run vctools to create conversion files from .vcf
-	if [ ! -f ${OUTPUT_PREFIX}.ped ] && [ ! -f ${OUTPUT_PREFIX}.map ]; then
-	   ### convert vcf to PLINK format (.ped and .map)
-	   vcftools --vcf ${INPUT_VCF} --plink  --out ${OUTPUT_PREFIX}
-	fi
-
-	### compute population allele frequencies 
-	python ${iADMIX_DIR}/runancestry.py               \
-	  --freq  ${iADMIX_DIR}/hapmap3.8populations.hg19 \
-	  --path=${iADMIX_DIR}                            \
-	  --plink ${OUTPUT_PREFIX}       \
-	  --out   ${OUTPUT_PREFIX}   
-
-done
+### compute population allele frequencies 
+python    ${iADMIX_DIR}/runancestry.py  --freq  ${RESOURCE_DATA} \
+  --path=${iADMIX_DIR} --plink ${OUTPUT_PREFIX} --out ${OUTPUT_PREFIX} 
